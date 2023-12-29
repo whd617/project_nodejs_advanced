87ed3dd89601ef2d951e1a65cc2c03c60f0096ba
Jonghyeon Choi <whd617@naver.com>
2023-12-29 12:45:17 +0900

    로그인, 회원가입 sequlize db를 prisma db로 변경

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -1,23 +1,21 @@
 import { PASSWORD_HASH_SALT_ROUNDS } from '../constants/security.constant.js';
 import bcrypt from 'bcrypt';
-import db from '../../models/index.cjs';
-const { Users } = db;
+import { prisma } from '../utils/prisma/index.js';
 
 export class UsersRepository {
   createOne = async ({ email, name, password }) => {
     const hashedPassword = bcrypt.hashSync(password, PASSWORD_HASH_SALT_ROUNDS);
 
-    const newUser = (
-      await Users.create({ email, password: hashedPassword, name })
-    ).toJSON();
+    const newUser = await prisma.user.create({
+      data: { email, password: hashedPassword, name },
+    });
     delete newUser.password;
-
     return newUser;
   };
 
   readOneByEmail = async (email) => {
-    const user = await Users.findOne({ where: { email } });
+    const user = await prisma.user.findUnique({ where: { email } });
 
-    return user?.toJSON();
+    return user;
   };
 }
